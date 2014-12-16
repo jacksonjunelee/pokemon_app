c6554dc3d28aa1021a7f518f43dc8549e0b05dac
jjlee5266 <jjlee5266@gmail.com>
2014-12-15 21:15:01 -0500

    switching pokemon

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -6,7 +6,13 @@ var game = {
   play: function(){
           console.log("Random " + this.stage[1].name + " appears");
           console.log(this.stage[0].username + ' sends out ' + this.stage[0].pokemons[0].name);
-        }
+        },
+  show:function(){
+    game.stage[0].pokeshow();
+  },
+  switch: function(){
+    this.stage[0].pokeshow();
+  }
 
 
 
