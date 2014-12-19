5f857214d33ff54a82c33edce8744b6a2e0ed890
jjlee5266 <jjlee5266@gmail.com>
2014-12-18 21:02:26 -0500

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,42 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Trainer.create(
-# username: "MCK"
-# )
-#
-# Pokemon.create(
-# name: "bulbasaur",
-# nickname: "SAUR",
-# api_ref: 1,
-# trainer_id: 1
-# )
-# Pokemon.create(
-# name: "charmander",
-# nickname: "CHAR",
-# api_ref: 4,
-# trainer_id: 1,
-# hp: 39,
-# faint: false,
-# battle_img: '/assets/pokemon-main-sprites/yellow/back/4.png'
-# )
+Trainer.create(
+username: "MCK"
+)
+
 Pokemon.create(
-name: "charmeleon",
+name: "Bulbasaur",
+nickname: "SAUR",
+api_ref: 1,
+trainer_id: 1,
+hp: 45,
+faint: false,
+speed: 45,
+position: 2,
+max_hp: 45
+)
+
+Pokemon.create(
+name: "Charmander",
+nickname: "CHAR",
+api_ref: 4,
+trainer_id: 1,
+hp: 39,
+faint: false,
+speed: 65,
+position: 3,
+max_hp: 39
+)
+
+Pokemon.create(
+name: "Charmeleon",
 nickname: "CHARSWAG",
 api_ref: 5,
 trainer_id: 1,
 hp: 58,
-faint: false
+faint: false,
+speed: 80,
+position: 1,
+max_hp: 58
 )
